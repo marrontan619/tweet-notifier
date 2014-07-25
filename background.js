4a2962957ebb5d82bc3500760fd36fa268688271
Ryota <marrontan619@gmail.com>
2014-07-25 20:00:00 +0900

    何度も作る必要のないインスタンスや変数を外に出した。

--- background.js
+++ background.js
@@ -1,32 +1,29 @@
-var createNotification = function() {
+var noop = function() {};
+var xhr = new XMLHttpRequest();
+xhr.responseType = 'document';
+xhr.onload = function() {
     'use strict';
-    var noop = function() {};
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'document';
-    xhr.onload = function() {
-        var tweetElements = this.response.querySelectorAll( 'div.original-tweet' );
-        for ( var i = 0; i < 5; i++ ) {
-            var icon = tweetElements[i].querySelector( '.js-retweet-text' ) ? 'retweet.jpg' : 'tweet.jpg';
-            var tweeter = tweetElements[i].dataset.name;
-            var tweet = tweetElements[i].querySelector( 'p.js-tweet-text.tweet-text' ).innerText;
-            chrome.notifications.create(
-                '',
-                {
-                    type: 'basic',
-                    iconUrl: icon,
-                    title: tweeter,
-                    message: tweet,
-                    isClickable: true
-                },
-                noop
-            );
-        }
-    };
-    xhr.open( 'GET', 'https://twitter.com/?lang=ja' );
-    xhr.send();
+    var tweetElements = this.response.querySelectorAll( 'div.original-tweet' );
+    for ( var i = 0; i < 5; i++ ) {
+        var icon = tweetElements[i].querySelector( '.js-retweet-text' ) ? 'retweet.jpg' : 'tweet.jpg';
+        var tweeter = tweetElements[i].dataset.name;
+        var tweet = tweetElements[i].querySelector( 'p.js-tweet-text.tweet-text' ).innerText;
+        chrome.notifications.create(
+            '',
+            {
+                type: 'basic',
+                iconUrl: icon,
+                title: tweeter,
+                message: tweet,
+                isClickable: true
+            },
+            noop
+        );
+    }
 };
 
 chrome.browserAction.onClicked.addListener(function() {
     'use strict';
-    createNotification();
+    xhr.open( 'GET', 'https://twitter.com/?lang=ja' );
+    xhr.send();
 });
