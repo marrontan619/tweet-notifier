cb7a67028760e09e24eaa5f2a7e594b779603b80
Ryota <marrontan619@gmail.com>
2014-07-25 11:58:47 +0900

    リスト形式で試してみたが、入る文字数が少なくツイート主でだいたい埋まるため没。

--- background.js
+++ background.js
@@ -2,25 +2,28 @@ var createNotification = function() {
     'use strict';
     var noop = function() {};
     var xhr = new XMLHttpRequest();
+    var item = [];
     xhr.responseType = 'document';
     xhr.onload = function() {
         var tweetElements = this.response.querySelectorAll( 'div.original-tweet' );
         for ( var i = 0; i < 5; i++ ) {
-            var icon = tweetElements[i].querySelector( '.js-retweet-text' ) ? 'retweet.jpg' : 'tweet.jpg';
-            var tweeter = tweetElements[i].dataset.name;
-            var tweet = tweetElements[i].querySelector( 'p.js-tweet-text.tweet-text' ).innerText;
-            chrome.notifications.create(
-                '',
-                {
-                    type: 'basic',
-                    iconUrl: icon,
-                    title: tweeter,
-                    message: tweet,
-                    isClickable: true
-                },
-                noop
-            );
+            item[i] = {
+                title: tweetElements[i].dataset.name,
+                message: tweetElements[i].querySelector( 'p.js-tweet-text.tweet-text' ).innerText
+            };
         }
+        chrome.notifications.create(
+            '',
+            {
+                type: 'list',
+                title: 'tweet',
+                message: 'contents',
+                iconUrl: './icon.png',
+                items: item,
+                isClickable: true
+            },
+            noop
+        );
     };
     xhr.open( 'GET', 'https://twitter.com/?lang=ja' );
     xhr.send();
