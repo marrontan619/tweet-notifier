bff3bdb18e2a0ec7740b1ff9c4ca8e2de30777ef
Ryota <marrontan619@gmail.com>
2014-07-25 20:00:00 +0900

    緊急時の一括削除ボタンを追加
    読み込みツイート数を使いまわすので、定数化

--- background.js
+++ background.js
@@ -1,25 +1,32 @@
-var noop = function() {};
-var xhr = new XMLHttpRequest();
+var noop = function() {},
+    xhr = new XMLHttpRequest(),
+    MAX_COUNT = 5;
 xhr.responseType = 'document';
 xhr.onload = function() {
     'use strict';
     var tweetElements = this.response.querySelectorAll( 'div.original-tweet' );
-    for ( var i = 0; i < 5; i++ ) {
+    for ( var i = 0; i < MAX_COUNT; i++ ) {
         var icon = tweetElements[i].querySelector( '.js-retweet-text' ) ? 'retweet.jpg' : 'tweet.jpg';
         var tweeter = tweetElements[i].dataset.name;
         var tweet = tweetElements[i].querySelector( 'p.js-tweet-text.tweet-text' ).innerText;
         chrome.notifications.create(
-            '',
+            String(i),
             {
                 type: 'basic',
                 iconUrl: icon,
                 title: tweeter,
                 message: tweet,
+                buttons: [{title: 'Clear'}],
                 isClickable: true
             },
             noop
         );
     }
+    chrome.notifications.onButtonClicked.addListener(function() {
+        for (var j = 0; j < MAX_COUNT; j++) {
+            chrome.notifications.clear(String(j), noop);
+        }
+    });
 };
 
 chrome.browserAction.onClicked.addListener(function() {
