1521214ad1d85d108ca27dcb7e3bef9f6f7e9956
Ryota <marrontan619@gmail.com>
2014-07-29 01:56:06 +0900

    オプションページを作成
    今のところ設定項目は4つ
    セットとゲットができただけで、動作に反映はしていない。

---
+++ options.js
@@ -0,0 +1,39 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    'use strict';
+    var storage = chrome.storage.local,
+        options = document.getElementById('options'),
+        maxTweet = document.getElementById('max-tweet'),
+        windowWidth = document.getElementById('window-width'),
+        windowHeight = document.getElementById('window-height'),
+        incognito = document.getElementById('is-incognito');
+
+    options.addEventListener( 'input', function() {
+        var item = {},
+            maxTweetVal = Math.min( maxTweet.value, 20 ),
+            windowWidthVal = Math.max( windowWidth.value, 1 ),
+            windowHeightVal = Math.max( windowHeight.value, 1 ),
+            isIncognito = incognito.checked;
+        item['tweet-notifier'] = {
+            'maxTweetVal': maxTweetVal,
+            'windowWidthVal': windowWidthVal,
+            'windowHeightVal': windowHeightVal,
+            'isIncognito': isIncognito
+        };
+        storage.set(item, function() {});
+    });
+
+    incognito.addEventListener( 'change', function() {
+        var inputEvent = new Event('input');
+        options.dispatchEvent( inputEvent );
+    });
+
+    storage.get( 'tweet-notifier', function( items ) {
+        if ( items !== null ) {
+            var item = items['tweet-notifier'];
+            maxTweet.value = item.maxTweetVal;
+            windowWidth.value = item.windowWidthVal;
+            windowHeight.value = item.windowHeightVal;
+            incognito.checked = item.isIncognito;
+        }
+    });
+});
